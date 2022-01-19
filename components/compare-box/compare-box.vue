5ba64d6afd226b2eea48581b4d9d5bb46b29321a
LevyDeng <ptrees@126.com>
2022-01-19 16:35:11 +0800

    complete store

--- components/compare-box/compare-box.vue
+++ components/compare-box/compare-box.vue
@@ -4,11 +4,10 @@
       <uni-col :span="11" id="left">
         <uni-row class="demo-uni-row">
           <uni-col :span="3">
-            <uni-row><button>置顶</button></uni-row>
-            <uni-row><button>置底</button></uni-row>
+            <uni-row class="set-bottom"><button>置底</button></uni-row>
           </uni-col>
           <uni-col :span="21">
-            <uni-card :title="characters[0]" :cover="leftImageUrl"></uni-card>
+            <uni-card @click="choose('left-box')" id="left-box" :title="challanger" :cover="leftImageUrl"></uni-card>
           </uni-col>
         </uni-row>
       </uni-col>
@@ -18,11 +17,10 @@
       <uni-col :span="11" id="right">
         <uni-row class="demo-uni-row">
           <uni-col :span="21">
-            <uni-card :title="characters[1]" :cover="rightImageUrl"></uni-card>
+            <uni-card :title="challangee" :cover="rightImageUrl"></uni-card>
           </uni-col>
           <uni-col :span="3">
-            <uni-row><button>置顶</button></uni-row>
-            <uni-row><button>置底</button></uni-row>
+            <uni-row class="set-bottom"><button>置底</button></uni-row>
           </uni-col>
         </uni-row>
       </uni-col>
@@ -31,9 +29,6 @@
 </template>
 
 <script>
-  function compare(event) {
-    
-  }
   //import store from '@/store/index.js'
   export default {
     data() {
@@ -41,32 +36,51 @@
         //characters: this.$store.state.characters
       }
     },
+    methods: {
+      choose: function (id) {
+        console.log(document.getElementById(id))
+      },
+    },
     computed: {
       characters() {
         return this.$store.state.characters
       },
+      challanger() {
+        return this.$store.state.challanger
+      },
+      challangee() {
+        return this.$store.state.challangee
+      },
       leftImageUrl() {
-        return "/static/"+this.$store.state.characters[0]+".png"
+        return "/static/" + this.$store.state.challanger + ".png"
       },
       rightImageUrl() {
-        return "/static/"+this.$store.state.characters[1]+".png"
+        return "/static/" + this.$store.state.challangee + ".png"
       }
+    },
+    created() {
+      this.$store.commit("initChallanger")
+      this.$store.commit("initChallangee")
+      this.$store.commit("addToSeq", this.$store.state.characters[1], 0)
     }
   }
 </script>
 
 <style>
   .demo-uni-row {
-      margin-bottom: 10px;
-      /* QQ、字节小程序文档写有 :host，但实测不生效 */
-      /* 百度小程序没有 :host，需要设置block */
-      /* #ifdef MP-TOUTIAO || MP-QQ || MP-BAIDU */
-      display: block;
-      /* #endif */
-      justify-content: center;
-      align-items: center;
+    margin-bottom: 10px;
+    /* QQ、字节小程序文档写有 :host，但实测不生效 */
+    /* 百度小程序没有 :host，需要设置block */
+    /* #ifdef MP-TOUTIAO || MP-QQ || MP-BAIDU */
+    display: block;
+    /* #endif */
+    justify-content: center;
+    align-items: center;
   }
-  uni-row {
-    
+
+  .set-bottom {
+    justify-content: flex-end;
+    align-items: flex-end;
+    display: flex;
   }
 </style>
--- store/index.js
+++ store/index.js
@@ -13,8 +13,49 @@ import { createStore } from 'vuex'
 const store = createStore({
 // #endif
 	state: {
-		characters: read_xlsx('static/characters.xlsx')
-	}
+		characters: read_xlsx('static/characters.xlsx'),
+    challanger: '',
+    challangee: '',
+    bottom: [],
+    seq: []
+	},
+  getters: {
+    getChallanger: state => {
+      var rand = Math.floor(Math.random()*state.characters.length)
+      return state.characters[rand]
+    },
+    getChallangee: state => {
+      //var rand = Math.floor(Math.random()*state.characters.length)
+      return state.characters[1]
+    }
+  },
+  mutations: {
+    removeCharacter (state, character) {
+      for (var i=0; i < state.characters.length; i++) {
+        if (state.characters[i] === character) {
+          state.characters.splice(i, 1)
+        }
+      }
+    },
+    addToBottom (state, character) {
+      state.characters.push(character)
+    },
+    addToSeq (state, character, n) {
+      state.characters.splice(n, 0, [character])
+    },
+    addToSeqTie (state, character, n) {
+      state.characters[n].push(character)
+    },
+    setChallangee (state, n) {
+      state.challangee = state.seq[n][0]
+    },
+    initChallanger (state) {
+      state.challanger = state.characters[0]
+    },
+    initChallangee (state) {
+      state.challangee = state.characters[1]
+    }
+  }
 })
 
 
