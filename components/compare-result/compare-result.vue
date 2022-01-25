8e5ab1b10f43203152af5d3db0e9c57dd44c1166
LevyDeng <ptrees@126.com>
2022-01-25 11:16:37 +0800

    add compare-result

---
+++ components/compare-result/compare-result.vue
@@ -0,0 +1,34 @@
+<template>
+  <view width="1024px">排行
+    <view v-for="characters, i in seq" :key="i">
+      <uni-row >
+        <uni-col :span="2">{{ i }}</uni-col>
+        <uni-col :span="8" v-for="c in characters" :key="c.id">
+          <uni-card :title="c" :cover="'/static/小图/'+c+'.png'">
+            
+          </uni-card>
+        </uni-col>
+      </uni-row>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"compare-result",
+    data() {
+      return {
+        
+      };
+    },
+    computed: {
+      seq() {
+        return this.$store.state.seq
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
